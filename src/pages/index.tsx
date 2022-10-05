8eac231ab078128c55c8e91c0f1dc4a25ff28dda
Masson LeMieux <lemieuxstudios@gmail.com>
2022-10-05 10:30:28 -0400

    changed name in index.tsx path from NavBar to navBar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import NavBar from "../../components/NavBar";
+import NavBar from "../../components/navBar";
 // import { trpc } from "../utils/trpc";
 // import styles from "./index.module.css";
 
