ffa3077c051faaa12622ce3ab2a3ebfcffc05bf3
Masson LeMieux <fml45678@gmail.com>
2022-10-19 17:47:03 -0400

    styled some components, added link to subscription sign up, and added media queries for mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import NavBar from "../../components/navBar";
 // import { trpc } from "../utils/trpc";
-// import styles from "./index.module.css";
+import styles from "./index.module.css";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -17,13 +18,15 @@ const Home: NextPage = () => {
         <div>
           <img src="" alt="" />
         </div>
-        <div>
+        <div className={styles.mainContent}>
           <h2>Ready to learn Guitar?</h2>
           <p>
             Registration is now open. Group Guitar classes begin November 6th!
           </p>
+          <Link href="https://www.paypal.com/webapps/billing/plans/subscribe?plan_id=P-2VF03638W82253700MLSHVZI">
+            <button className={styles.signUp}>Sign up today</button>
+          </Link>
         </div>
-        <span>Sign up today</span>
       </div>
     </>
   );
