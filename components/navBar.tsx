94dec1e3173808467ee8391b49e90a6391fd3657
fml45678 <84721089+fml45678@users.noreply.github.com>
2022-10-23 19:13:03 -0400

    installed prisma which may have been a mistake, and added info to signUp page, added links to proper pages in nav

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,21 +1,37 @@
 import type { NextPage } from "next";
 import styles from "./navBar.module.css";
+import Link from "next/link";
+
 const NavBar: NextPage = () => {
   return (
     <>
       <div className={styles.container}>
         <div>
-          <h1 className={styles.title}>LEMIEUX STUDIOS GUITAR</h1>
+          <Link href="/">
+            <h1 className={styles.title}>LEMIEUX STUDIOS GUITAR</h1>
+          </Link>
         </div>
         <ul className={styles.lists}>
-          <li>About</li>
-          <li>Calendar</li>
-          <li>Forum</li>
-          <li>Contact</li>
+          <Link href="/about">
+            <li>About</li>
+          </Link>
+          <Link href="/calendar">
+            <li>Calendar</li>
+          </Link>
+          <Link href="/forum">
+            <li>Forum</li>
+          </Link>
+          <Link href="/contact">
+            <li>Contact</li>
+          </Link>
         </ul>
         <ul className={styles.lists}>
-          <li>Login</li>
-          <li>Join</li>
+          <Link href="/api/auth/signin">
+            <li>Login</li>
+          </Link>
+          <Link href="/signUp">
+            <li>Join</li>
+          </Link>
         </ul>
       </div>
     </>
--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import NavBar from "../../components/navBar";
 // import { trpc } from "../utils/trpc";
-// import styles from "./index.module.css";
+import styles from "./index.module.css";
 // import Link from "next/link";
 
 const SignUp: NextPage = () => {
@@ -14,7 +14,15 @@ const SignUp: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar />
-      <div>SignUp</div>
+      <div>
+        <h2>Choose your lesson day</h2>
+        <div className={styles.days}>
+          <button>Sundays</button>
+          <button>Mondays</button>
+          <button>Tuesdays</button>
+        </div>
+        <h3>Thurdays are review days</h3>
+      </div>
     </>
   );
 };
