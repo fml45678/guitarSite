a982f3eee0d6bc537fbe62ec7e534565ada43109
fml45678 <84721089+fml45678@users.noreply.github.com>
2022-10-25 22:51:17 -0400

    added more info to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -24,6 +24,51 @@ const About: NextPage = () => {
           Learning guitar in a group environment is an enjoyable and motivating
           experience.
         </p>
+
+        <span className={styles.rulesTitle}>Group Guitar Lessons Rules</span>
+
+        <ul className={styles.rules}>
+          <li>Classes are 1 hour</li>
+          <li>
+            You should bring your guitar and instructional material to each
+            class
+          </li>
+          <li>
+            You are expected to practice the lesson material between classes to
+            keep up with the group, adequate help will be available between
+            classes for all registered students.
+          </li>
+          <li>
+            If you miss a lesson or cannot make your lesson time, you can come
+            to another lesson during the same week if available. Lesson material
+            will also be made available online to registered students
+          </li>
+          <li>
+            Payments will be drafted monthly and must be paid before attending a
+            lesson
+          </li>
+          <li>Refunds are not permitted</li>
+          <li>
+            You may cancel lessons at any time and will not be charged for the
+            remaining months
+          </li>
+        </ul>
+
+        <span className={styles.rulesTitle}>
+          What Students need for lessons
+        </span>
+
+        <ul className={styles.rules}>
+          <li>
+            Guitar (preferably acoustic or classical but electric is fine)
+          </li>
+          <li>Pick</li>
+          <li>Strap or footstool</li>
+          <li>Instructional book</li>
+          <li>Pencil</li>
+          <li>Tuner (optional)</li>
+          <li>Capo (optional)</li>
+        </ul>
       </div>
     </>
   );
