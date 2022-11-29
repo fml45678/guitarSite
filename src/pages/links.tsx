8bcaf5b27059962e90f6748a4e8a6a55fc1c55f7
fml45678 <84721089+fml45678@users.noreply.github.com>
2022-11-29 17:56:39 -0500

    added remaining links to linkTree

--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -6,7 +6,7 @@ import styles from "./links.module.css";
 
 const Links: NextPage = () => {
   return (
-    <>
+    <div className={styles.containerContainer}>
       <Head>
         <title>All of our stuff</title>
         <meta name="description" content="Links" />
@@ -18,43 +18,43 @@ const Links: NextPage = () => {
         <div className={styles.groupContainer}>
           <a href="https://etsy.com/shop/treelyland">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>Treely Land</span>
               <img className={styles.logo} src="treelyLandLogo.jpg" alt="" />
+              <span className={styles.title}>Treely Land</span>
             </div>
           </a>
           <a href="https://etsy.com/shop/spongeprintstudio">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>Sponge Print Studio</span>
               <img className={styles.logo} src="spongePrintLogo.jpg" alt="" />
+              <span className={styles.title}>Sponge Print Studio</span>
             </div>
           </a>
           <a href="https://etsy.com/shop/lemieuxstudios">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>T-Shirts</span>
               <img className={styles.logo} src="tShirts.jpg" alt="" />
+              <span className={styles.title}>T-Shirts</span>
             </div>
           </a>
           <a href="https://etsy.com/shop/usemidi">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>Midi Instruments</span>
               <img className={styles.logo} src="midi.jpg" alt="" />
+              <span className={styles.title}>Midi Instruments</span>
             </div>
           </a>
           <a href="https://lemieuxstudios.com">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>Guitar Lessons</span>
               <img className={styles.logo} src="guitarLessons.jpg" alt="" />
+              <span className={styles.title}>Guitar Lessons</span>
             </div>
           </a>
           <a href="https://massonlemieux.com/podcast">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>Podcast Sponsor</span>
               <img className={styles.logo} src="podcast.jpg" alt="" />
+              <span className={styles.title}>Podcast Sponsor</span>
             </div>
           </a>
         </div>
         <h2 className={styles.headers}>Donation Links!!</h2>
-        <div className={styles.groupContainer}>
+        <div className={styles.groupContainerDonate}>
           <a href="https://www.paypal.com/donate/?hosted_button_id=8JGJVSZ9ZCYRY">
             <div>
               {/* <span className={styles.title}>PayPal</span> */}
@@ -72,112 +72,114 @@ const Links: NextPage = () => {
         <div className={styles.groupContainer}>
           <a href="https://www.youtube.com/@lemieuxstudios6476">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>LeMieux Studios Youtube</span>
               <img className={styles.logo} src="youtubeLogo.png" alt="" />
+              <span className={styles.title}>LeMieux Studios Youtube</span>
             </div>
           </a>
           <a href="https://www.youtube.com/@lifemattersamplingstation4844">
             <div className={styles.itemContainer}>
+              <img className={styles.logo} src="youtubeLogo.png" alt="" />
               <span className={styles.title}>
                 Life Matter Sampling Station Youtube
               </span>
-              <img className={styles.logo} src="youtubeLogo.png" alt="" />
             </div>
           </a>
           <a href="https://www.youtube.com/channel/UC_Vy5Ay28Op5LbMVINnsR8Q">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>Masson LeMieux Youtube</span>
               <img className={styles.logo} src="youtubeLogo.png" alt="" />
+              <span className={styles.title}>Masson LeMieux Youtube</span>
             </div>
           </a>
           <a href="https://massonlemieux.com/podcast">
             <div className={styles.itemContainer}>
+              <img className={styles.logo} src="podcast.jpg" alt="" />
               <span className={styles.title}>
                 The Masson LeMieux Show: <br /> Sports Talk Podcast
               </span>
-              <img className={styles.logo} src="podcast.jpg" alt="" />
             </div>
           </a>
-          <a href="">
+          <a href="https://www.instagram.com/fml45678/">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>Masson Instagram</span>
               <img className={styles.logo} src="InstagramLogo.png" alt="" />
+              <span className={styles.title}>Masson's Instagram</span>
             </div>
           </a>
-          <a href="">
+          <a href="https://www.instagram.com/yiswork/">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>Yiseul Instagram</span>
               <img className={styles.logo} src="InstagramLogo.png" alt="" />
+              <span className={styles.title}>Yiseul's Instagram</span>
             </div>
           </a>
-          <a href="">
+          <a href="https://www.tiktok.com/@massonlemieux">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>Masson Tiktok</span>
               <img className={styles.logo} src="TikTokLogo.png" alt="" />
+              <span className={styles.title}>Masson's Tiktok</span>
             </div>
           </a>
-          <a href="">
+          <a href="https://www.tiktok.com/@spongeprintstudio">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>Yiseul Tiktok</span>
               <img className={styles.logo} src="TikTokLogo.png" alt="" />
+              <span className={styles.title}>Yiseul's Tiktok</span>
             </div>
           </a>
-          <a href="">
+          <a href="https://music.apple.com/us/album/summer-to-spring/1552966853">
             <div className={styles.itemContainer}>
+              <img className={styles.logo} src="summer.jpg.webp" alt="" />
               <span className={styles.title}>Summer to Spring Album</span>
-              <img className={styles.logo} src="" alt="" />
             </div>
           </a>
-          <a href="">
+          <a href="https://massonlemieux.com">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>Masson Website</span>
               <img className={styles.logo} src="" alt="" />
+              <span className={styles.title}>Masson's Website</span>
             </div>
           </a>
-          <a href="">
+          <a href="https://yiseullemieux.com">
             <div className={styles.itemContainer}>
-              <span className={styles.title}>Yiseul Website</span>
               <img className={styles.logo} src="" alt="" />
+              <span className={styles.title}>Yiseul's Website</span>
             </div>
           </a>
         </div>
         <h2 className={styles.headers}>Upcoming Stuff!!!!</h2>
-        <div>
-          <span className={styles.title}>Puppet Shows</span>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <span className={styles.title}>New Music</span>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <span className={styles.title}>Coffee Company</span>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <span className={styles.title}>New Films</span>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <span className={styles.title}>Making New Musical Instruments</span>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <span className={styles.title}>
-            A new(consumer(not seller)centric) Amazon
-          </span>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <span className={styles.title}>Making Clothing</span>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <span className={styles.title}>Immersive Exhibitions</span>
-          <img src="" alt="" />
+        <div className={styles.upcoming}>
+          <div>
+            <span className={styles.title}>Puppet Shows</span>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <span className={styles.title}>New Music</span>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <span className={styles.title}>Coffee Company</span>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <span className={styles.title}>New Films</span>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <span className={styles.title}>Making New Musical Instruments</span>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <span className={styles.title}>
+              A new(consumer(not seller)centric) Amazon
+            </span>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <span className={styles.title}>Making Clothing</span>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <span className={styles.title}>Immersive Exhibitions</span>
+            <img src="" alt="" />
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
